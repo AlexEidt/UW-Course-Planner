5d5511332179b8bf595420d2a69775336020ad1d
Alex Eidt <alex.eidt@outlook.com>
2019-07-20 20:38:50 -0700

    Add scripts for web scraping and data processing

---
+++ parse_courses.py
@@ -0,0 +1,293 @@
+"""
+MIT License
+
+Copyright (c) 2017 Kamil Jiwa
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*****************************************************************************
+NOTE from Alex Eidt:
+All lines of code (or fragments) from Kamil Jiwa's repository 'uw-course-catalog' used in this
+script are marked with a '#KJ' comment at the far right end of the line.
+'uw-course-catalog' repository on GitHub: https://github.com/kjiwa/uw-course-catalog
+'Kamil Jiwa' on GitHub:                   https://github.com/kjiwa
+"""
+
+# Creates a tsv file containing course data for each UW Campus
+
+import re
+import csv
+import time
+import urllib3 as URL
+from bs4 import BeautifulSoup as Soup
+from tqdm import tqdm
+
+
+CAMPUSES = {                                                                                #KJ
+    'Bothell': 'http://www.washington.edu/students/crscatb/',                               #KJ
+    'Seattle': 'http://www.washington.edu/students/crscat/',                                #KJ
+    'Tacoma': 'http://www.washington.edu/students/crscatt/'                                 #KJ
+}                                                                                           #KJ
+
+
+# Replaces all occurances of un-numbered courses in the given course description
+# 'description': The course description
+def complete_description(description):
+    find_all = re.findall(r'[A-Z& ]+/\s?[A-Z& ]+/\s?[A-Z& ]+\s?\d+', description)
+    find_one = re.findall(r'[A-Z& ]+/\s?[A-Z& ]+\s?\d+', description)
+    
+    def remove_in(a, b):
+        remove = []
+        for x in a: 
+            for y in b: 
+                if y in x: remove.append(y)
+        for x in b: a.append(x)
+        for x in remove: a.remove(x)
+        result = []
+        for x in a:
+            result.append(x)
+        return result
+            
+    if len(find_one) is not 0:
+        find_all = remove_in(find_all, find_one)
+        for i, x in enumerate(find_all):
+            completed = []
+            old = find_all[i]
+            find_all[i] = x.replace(' ', '')
+            number = re.search(r'\d{3}', find_all[i]).group(0)
+            for crs in re.findall(r'[A-Z& ]+', find_all[i]):
+                completed.append('{}{}'.format(crs, number))
+            description = description.replace(old, '/'.join(completed), 1)
+    find_series_2 = re.findall(r'[A-Z& ]+\d+, ?\d+', description)
+    find_series_3 = re.findall(r'[A-Z& ]+\d+, \d+, ?\d+', description)
+    find_series_2 = remove_in(find_series_3, find_series_2)
+    for i, series in enumerate(find_series_2):
+        find_series_2[i] = series.replace(' ', '')
+        number = find_series_2[i][-3:]
+        depmnt = None
+        course_dep = re.search(r'[A-Z& ]+', find_series_2[i])
+        if course_dep: 
+            depmnt = course_dep.group(0)
+            description = description.replace(series, '{}{}'.format(depmnt, number), 1)
+    return description
+
+
+# Returns the number of credits for the course if there are any
+# Otherwise returns an empty String
+# 'description': The course description
+def get_credits(description):
+    match = re.search(r'\(\[?\d', description)
+    return int(match.group(0).replace('(', '').replace('[', '')) if match else ''
+
+
+# Returns the prerequisite courses for the course if there are any
+# Otherwise returns an empty String
+# 'description': The course description
+prereq_regex = re.compile(r';|:|and either|and one of')
+def get_requisites(description, type):
+    if type not in description:                                                             #KJ
+        return ''                                                                           #KJ   
+    parts = description.split('Offered:')[0].split(type)                                    #KJ
+    POI = ''
+    if 'permission of' in parts[1].lower(): POI = 'POI'                        
+    required = prereq_regex.split(parts[1].split('(')[0])
+    result = ''
+
+    def remove_unnessecary(course_sub):
+        course_sub = re.sub(r'(E|e)ither', '', course_sub)
+        course_sub = re.sub(r'[a-z]+', '', course_sub)
+        course_sub = re.sub(r'\d?\.\d', '', course_sub)
+        return course_sub
+
+    def split_courses(required_list, join_char, split_char, resulting):
+        for course_option in required_list:
+            reqcrs = [crs.strip() for crs in re.findall(r'([A-Z& ]+\s?\d+)', course_option)]
+            resulting += join_char.join(list(filter(('').__ne__, reqcrs)))
+            resulting += split_char
+        return resulting
+
+    for course in required:
+        if 'either' in course or 'Either' in course:
+            if 'with either' in course:
+                with_either = course.split('with either')
+                course = '{}{}{}'.format(with_either[0], '&&', with_either[1].replace('or', '/'))
+            reqs = course.split('or')
+            if 'and' in course:
+                for c in reqs:
+                    c = c.replace('and', '&&').replace(' ', '')
+                    c = remove_unnessecary(c)
+                    result += c
+                    result += ','
+            else:
+                result = split_courses(reqs, ',', ',', result)
+        elif 'both' in course or 'in each of' in course:
+            result = split_courses(course.split('or'), '&&', ',', result)
+        elif 'minimum grade' in course:
+            course = course.replace('and', '&&').replace('or', ',')
+            course = remove_unnessecary(course)
+            result += course
+        elif 'and' in course and 'or' not in course:
+            result = split_courses(course.split('and'), ',', ';', result)
+        else:
+            course = course.replace('and', '&&').replace(' ', '').replace('/', ',').replace('or', ',')
+            course = remove_unnessecary(course)
+            remove = []
+            course_list = course.split(',')
+            for i, x in enumerate(list(filter(('').__ne__, course_list))):
+                match = re.search(r'[A-Z& ]+\s?\d{3}(&&[A-Z& ]+\s?\d{3})?', x)
+                if not match: remove.append(x)
+                else: course_list[i] = match.group(0)
+            for x in remove: 
+                try: course_list.remove(x)
+                except ValueError: pass
+            result += ','.join(course_list)
+            result += ','
+        result += ';'
+    result += ',{}'.format(POI)
+    result = result.replace(',;', ';').replace(';,', ';')
+    result = result.replace(';&&', ';').replace('&&;', ';')
+    result = result.replace(',&&', ',').replace('&&,', ',')
+    result = re.sub(r';+', ';', result)
+    result = re.sub(r',+', ',', result)
+    result = re.sub(r';\d,', ';', result)
+    result = re.sub(r',\d,', ',', result)
+    result = re.sub(r';\d&', ';&', result)
+    result = re.sub(r',\d&', ',&', result)
+    if result[0] in '1234567890':
+        result = result[1:]
+    result = result.replace(',;', ';').replace(';,', ';')
+    result = re.sub(r';+', ';', result)
+    result = re.sub(r',+', ',', result)
+    result = result.replace(' ', '').strip(';').strip(',').strip('\"').strip('.')
+    return ','.join(list(dict.fromkeys(result.split(','))))
+
+
+# Returns the Quarters that the course is offered if there are any
+# Otherwise returns an empty String
+# 'description': The course description
+def get_offered(description):
+    if 'Offered:' not in description:                                                       #KJ
+        return ''                                                                           #KJ
+    check_parts = description.split('Offered: ')[1]                                         #KJ
+    parts = check_parts.split(';')[1] if ';' in check_parts else check_parts
+    parts = re.sub(r'([A-Z& ]+\s?\d+)', '', parts)
+    result = []
+    quarters = ['A', 'W', 'Sp', 'S']
+    for quarter in quarters:
+        if quarter in parts:
+            result.append(quarter)
+            parts = parts.replace(quarter, '')
+    return ','.join(result)
+    
+
+# Returns the courses offered jointly with the course if there are any
+# Otherwise returns an empty String
+# 'description': The course description
+def get_jointly(description):
+    if 'jointly with' not in description:                                                   #KJ
+        return ''
+    parts = description.split('jointly with ')[1].split(';')                                #KJ
+    course = ''
+    for course_option in list(filter(('').__ne__, parts)):
+        reqcrss = [crs.strip() for crs in re.findall(r'([A-Z& ]+\s?\d+)', course_option)]     #KJ
+        course += ','.join(reqcrss)                                                         
+        course += ';'
+    course = course.replace(',;', ';')
+    course = re.sub(r';+', ';', course)
+    return course.replace(' ', '').strip(';').strip()                            
+
+
+# Returns the credit types offered for the course if there are any
+# Otherwise returns an empty String
+# 'description': The course description
+def get_credit_types(description):
+    match = re.search(r'((I&S|DIV|NW|VLPA|QSR)(,?\s?/?))+', description)
+    return match.group(0).strip() if match else ''
+
+
+# Returns the course name
+# Otherwise returns an empty String
+# 'description': The course description
+def get_name(description):
+    match = re.search(r'[^\(]+', description)
+    return re.sub(r'([A-Z& ]+\s?\d+)', '', match.group(0)).strip() if match else ''
+
+
+# Returns a list of all the information for the given course in the 'description'
+# 'campus': The campus the course is offered
+# 'department': The abbreviated department name that the course is offered in
+# 'number': The course number
+# 'description': The course description
+def get_course_data(campus, department, number, description):
+    functions = [get_name, get_credits, get_credit_types, get_offered, get_jointly]
+    description = description.split('View course details in MyPlan:')[0]
+    description = complete_description(description)
+    data = [campus, department, number]
+    for function in functions:
+        data.append(function(description))
+    data.append(get_requisites(description, 'Prerequisite:'))
+    data.append(get_requisites(description, "Co-requisite"))
+    data.append(description)
+    return data
+
+
+# Returns a BeautifulSoup object for the given link
+# 'website_link': The website to be web scraped
+def get_web_soup(website_link):
+    website = URL.PoolManager()
+    source = website.request('GET', website_link)
+    return Soup(source.data, features='lxml')
+
+
+# Creates a .tsv file with all the course offered on the given 'campus'
+# 'course_data': BeautifulSoup object with the department list website for the given 'campus'
+# 'campus': The campus to get the courses from
+# 'path': The file path to the directory storing all .tsv files for each campus
+def read_department_courses(course_data, campus, path):
+    with open('{}\\{}\\{}.tsv'.format(path, 'TSV', campus), mode='w', newline='') as courses:
+        column_names = ['Campus', 'Department Name', 'Course Number', 'Course Name', 'Credits',
+                        'Areas of Knowledge', 'Quarters Offered', 'Offered with', 
+                        'Prerequisites', 'Co-Requisites', 'Description']
+        csv_writer = csv.writer(courses, delimiter='\t')
+        csv_writer.writerow(column_names)
+        for dep_link in tqdm(course_data.find_all('a')):
+            dep_file = dep_link.get('href')
+            if '.html' in dep_file and '/' not in dep_file:
+                file_name = '{}{}'.format(CAMPUSES[campus], dep_file)   
+                department = get_web_soup(file_name)
+                for course in department.find_all('a'):
+                    course_ID = course.get('name')             
+                    department_name = None
+                    course_number = None
+                    if course_ID:
+                        department_name = re.sub(r'\d+', '', course_ID).upper()
+                        course_number = re.sub(r'[A-Z]+', '', course_ID.upper())
+                        csv_writer.writerow(get_course_data(campus, department_name, 
+                                            course_number, course.get_text()))
+
+
+# Creates a .tsv file for every UW campus
+# 'path': The file path to the directory storing all .tsv files for each campus
+def gather(path):
+    t1 = time.perf_counter()
+    for campus, link in CAMPUSES.items():
+        read_department_courses(get_web_soup(link), campus, path)
+    t2 = time.perf_counter()
+    print('Course Catalogs for {} have been sucessfully created in: \n\t{}'.format(str(list(CAMPUSES.keys()))[1:-1].replace("'", ''), path))
+    print('Script ran in {:.1f} seconds'.format(t2 - t1))
---
+++ show_prereqs.py
@@ -0,0 +1,175 @@
+import csv
+import os
+import re
+import json
+from datetime import datetime
+from parse_courses import gather, CAMPUSES
+
+
+course_dir = 'UW_Campus_Catalogs' # Name of Folder with Campus tsv/json files
+course_files_dir = '{}\\{}'.format(os.getcwd(), course_dir) # course_dir file path
+
+
+# Creates a directory called 'course_dir' in the current directory if it does
+# not already exist. Initializes this directory with all missing campus files
+# if the directory had just been created. If the directory was already present,
+# the user can choose to update it.
+def check_files():
+    try:
+        os.mkdir(course_dir)
+        print('Created Directory {} in {}'.format(course_dir, os.getcwd()))
+        try: os.mkdir('{}\\{}'.format(course_files_dir, 'JSON'))
+        except Exception: pass
+        try: os.mkdir('{}\\{}'.format(course_files_dir, 'TSV'))
+        except Exception: pass
+    except Exception:
+        last_updated = str(datetime.fromtimestamp(os.stat(course_files_dir).st_mtime)).split('.')[0]
+        print('Course catalogs were last updated on {}'.format(last_updated))
+        update = input('Would you like to update the Course Catalogs? (y/n): ')
+    else:
+        update = 'y'
+    finally:
+        if 'y' in update.lower():
+            try: os.remove('{}\\{}\\{}'.format(course_files_dir, 'JSON', 'Total.json'))
+            except (FileExistsError, FileNotFoundError) as e: pass
+            try: os.remove('{}\\{}\\{}'.format(course_files_dir, 'TSV', 'Total.tsv'))
+            except (FileExistsError, FileNotFoundError) as e: pass
+            print('Creating TSV/JSON docs for {} UW Campuses...'.format(str(list(CAMPUSES.keys()))[1:-1].replace("'", '')))
+            gather(course_files_dir)
+            for campus in CAMPUSES.keys():
+                courses_dict = read_file(campus)
+                write_json(courses_dict, 'w', campus)
+                write_json(courses_dict, 'a', 'Total')
+                with open('{}\\{}\\{}.tsv'.format(course_files_dir, 'TSV', campus)) as campus_file:
+                    with open('{}\\{}\\{}.tsv'.format(course_files_dir, 'TSV', 'Total'), mode='a') as total:
+                        for line in campus_file:
+                            total.write(line)
+
+
+# Scans the tsv file for the given 'campus'
+# 'campus': The campus to search courses from
+# Returns a dictionary of all course ID's with their information
+def read_file(campus):
+    all_courses = {}
+    to_delete = ['MATH098', 'MATH103', 'MATH102', 'MATH100', 'MATH109', 'MATH120',
+                 'CHEM110']
+    try:
+        with open('{}\\{}\\{}.tsv'.format(course_files_dir, 'TSV', campus), mode='r') as file:
+            reader = csv.reader(file, delimiter='\t')
+            column_names = next(reader)
+            for course in reader: 
+                    all_courses['{0[1]}{0[2]}'.format(course)] = {
+                        column_names[i]: course[i] for i in range(len(column_names))
+                    }
+        for course in to_delete:
+            if course in all_courses:
+                del all_courses[course]  
+    except (FileExistsError, FileNotFoundError) as e:
+        print(e)
+        return {}
+    else:
+        return all_courses
+
+
+# Recursively prints all the prerequisite courses for the given 'course'
+# 'course_dict': The dictionary of courses
+# 'course': The course in question
+# 'indent': The indentation level for each prerequisite course
+def show_prerequisites(course_dict, courses_taken, course, indent):
+    if course is not '' and course in course_dict and course not in courses_taken:  
+        check = course_dict[course]['Prerequisites'].split(';')
+        for req in check:
+            list_reqs = re.compile(r',|&&|/').split(req)
+            #list_reqs = req.split(',')
+            if set(list_reqs).isdisjoint(courses_taken):
+                for reqcourse in list_reqs:
+                    if reqcourse is not '':
+                        branch = '{}{}'.format(indent, reqcourse)
+                        if len(branch.strip()) is not 1 and len(branch.strip().replace('|', '')) is not 0:
+                            if 'CHEM110' not in branch and 'MATH120' not in branch:
+                                if len(list_reqs) <= 1 and not '&&' in req:
+                                    print(branch) 
+                                elif reqcourse in find_and(req.split(',')).split('&&'):
+                                    print('{}&'.format(branch))
+                                else:
+                                    print('{}*'.format(branch))
+                        show_prerequisites(course_dict, courses_taken, reqcourse, indent + '|   ')
+                if len(check) > 1 and len(list_reqs) > 1:
+                    print(indent.replace('|   ', '', 1))
+
+
+# Returns the course group that is split by '&&'
+# 'req': The list of course groups
+def find_and(req):
+    for course in req:
+        if '&&' in course:
+            return course
+    return ''
+
+
+# Asks the user if their transcript should be scanned in to remove classes from the class
+# tree that they've already taken. 
+# 'course_dict': The dictionary of courses scanned in from the campus selected by the user
+# Returns a list of courses taken from the transcript given
+def scan_transcript(course_dict):
+    print('If you would like to scan in your transcript to eliminate searching courses you have')
+    print("already taken, go to MyUW under 'Unofficial Transcript' and press CTRL+A and paste")
+    print('your transcript into a text file in the same directory as this script.')
+    transcript = input('Scan in Transcript? (y/n): ')
+    file_name = ''
+    if 'y' in transcript:
+        check = False
+        while not check:
+            file_name = input('Transcript file name: ')
+            check = file_name in os.listdir(os.getcwd())
+    else:
+        return []
+    courses_taken = []
+    try:
+        with open(file_name, mode='r') as transcript:
+            for line in transcript:
+                if 'CUMULATIVE CREDIT SUMMARY' in line:
+                    break
+                match = re.search(r'([A-Z& ]+\s?\d{3})', line)
+                if match:
+                    dropped = re.search(r'\d\.\d\s+W\d\s', line)
+                    line_data = match.group(0).replace(' ', '')
+                    if line_data in course_dict and not dropped:
+                        courses_taken.append(line_data)
+    except (FileExistsError, FileNotFoundError) as e:
+        print(e)
+        return []
+    else:
+        return courses_taken
+
+
+# Writes the complete course dictionary for a UW Campus to a JSON file
+# 'all_courses': Course dictionary
+# 'campus': The campus the courses are from
+def write_json(all_courses, mode_type, campus):
+    file_name = '{}\\{}\\{}.json'.format(course_files_dir, 'JSON', campus)
+    with open(file_name, mode=mode_type) as json_file:
+        json.dump(all_courses, json_file, indent=4, sort_keys=True)
+
+
+# Creates the campus tsv files if necessary and prompts the user for a UW
+# Campus and the course they would like to search for
+def start():
+    check_files()
+    print('Creating JSON Files for {} UW Campuses...'.format(str(list(CAMPUSES.keys()))[1:-1].replace("'", '')))
+    for num, campus in enumerate(CAMPUSES.keys(), start=1):
+        print(campus)
+    campus = ''
+    while campus not in CAMPUSES.keys():
+        campus = input('Choose a UW Campus: ')
+    all_courses = read_file(campus)
+    courses_taken = scan_transcript(all_courses)
+    more = 'y'
+    while 'y' in more:
+        course = input('Enter a Course: ')
+        print('****************************************************')
+        show_prerequisites(all_courses, courses_taken, course, '')
+        more = input('Continue? (y/n): ').lower()
+
+
+start()
